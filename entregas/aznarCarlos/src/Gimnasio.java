cec972a616d7d2006162f7ae20b5db88b09d8971
Cacarlitros <carlos.aznar@alumnos.uneatlantico.es>
2026-04-06 20:28:33 +0200

    Damos Formato al Codigo y  creamos la vista publica de Actividad

--- entregas/aznarCarlos/src/Gimnasio.java
+++ entregas/aznarCarlos/src/Gimnasio.java
@@ -1,16 +1,29 @@
-class Gimnasio{ 
+class Gimnasio {
+
+    class Gimnasio {
 
 private string NombreGimnasio ("FitLifeCenter"){}
+
 private string Actividad ("yoga","spinnig","pilates","aquagym"){}
+
 private string Socios ("Juan Garcia","Gustavo Fring","Maria Lopez","Saul Perez"){}
+
 private string Monitores ("Carlos Lopez","Maria Martinez","Ana Sobremazas","Carmen Santamaria"){}
+
 private string DNI ("12345678A","87654321B","79189017K","11223344C"){}
+
 private double Aforo (1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20){}
+
 private string Fechas ("Lunes/Viernes","Martes/Jueves","Miercoles","Miércoles/Viernes"){}
+
 private boolean EstadoSuscripcion (Activo/inactivo){}
+
 private string Motivo (/…){}
 
-public void Actividad (Actividad,Monitores,Fechas,Aforo)
+        public void Actividad(String Actividad, String Monitores, String Fechas, int[] Aforo) {
+            System.out.println("[INFO] Clase de " + Actividad + " programada para los " + Fechas + ".");
+            System.out.println("       Monitor asignado: " + Monitores + ".");
+        }
 
 public void  Socios (Socios,DNI,EstadoSuscripcion)
 
@@ -18,9 +31,10 @@ public void reservas (Socios,Actividad)
 
 public void CancelarReserva (Socio,Actividad,Motivo)
 
-public Gimnasio(string NombreGimnasio, int[] Aforo) {
+public Gimnasio (string NombreGimnasio, int[] Aforo) {
   System.out.println(" Gimnasio '" + NombreGimnasio + "' abierto. Aforo máximo: " + Aforo.length + " personas.");
     }
+
 public void Monitor (Monitor,Actividad)
 
 public void EstadoReserva (Socio,Actividad,EstadoReserva)
